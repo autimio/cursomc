3b05d0d0e6fb195b00bda7ed75bdf7312ecbe370
Autímio Filho <autimio.filho@sgs.com>
2019-01-03 13:58:11 -0200

    Testando classe de dominio - "Categoria"

--- src/main/java/com/autimio/mc/resources/CategoriaResource.java
+++ src/main/java/com/autimio/mc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.autimio.mc.resources;
 
+import com.autimio.mc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String Listar () {
-        return "Rest está funcionando!";
+    public List<Categoria> Listar () {
+
+        Categoria cat1 = new Categoria(1,"teste 1");
+        Categoria cat2 = new Categoria(2, "teste 2");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
