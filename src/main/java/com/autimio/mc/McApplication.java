87c44802b749e604f6f3623db8ffcdc6ddcb3060
Autímio Filho <autimio.filho@sgs.com>
2019-02-26 16:10:23 -0300

    Add Cidades e Estados

--- src/main/java/com/autimio/mc/McApplication.java
+++ src/main/java/com/autimio/mc/McApplication.java
@@ -1,8 +1,12 @@
 package com.autimio.mc;
 
 import com.autimio.mc.domain.Categoria;
+import com.autimio.mc.domain.Cidade;
+import com.autimio.mc.domain.Estado;
 import com.autimio.mc.domain.Produto;
 import com.autimio.mc.repositories.CategoriaRepository;
+import com.autimio.mc.repositories.CidadeRepository;
+import com.autimio.mc.repositories.EstadoRepository;
 import com.autimio.mc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,13 @@ public class McApplication implements CommandLineRunner {
 
     @Autowired
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(McApplication.class, args);
@@ -41,8 +52,22 @@ public class McApplication implements CommandLineRunner {
         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
         p3.getCategorias().addAll(Arrays.asList(cat1));
 
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null,"Uberlância", est1);
+        Cidade c2 = new Cidade(null,"São Paulo", est2);
+        Cidade c3 = new Cidade(null,"Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+
         categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
         produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
     }
 }
 
